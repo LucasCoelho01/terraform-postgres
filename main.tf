469b3166355b86722ffb43a05e976d44de61ec9f
Lucas Coelho dos Santos <lucas.goleiro1995@gmail.com>
2024-04-30 21:55:23 -0300

    Ajusted terraform file

--- main.tf
+++ main.tf
@@ -1,19 +1,11 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_db_instance" "db" {
-  allocated_storage    = 2
-  storage_type         = "gp2"
+resource "aws_db_instance" "postgresdb" {
+  allocated_storage    = 10
+  db_name              = "postgresdb"
   engine               = "postgres"
-  engine_version       = "13.4"
-  instance_class       = "db.t2.micro"
-  name                 = "postgres-db"
+  engine_version       = "15"
+  instance_class       = "db.t3.micro"
   username             = "postgres"
   password             = "postgres"
-  parameter_group_name = "default.postgres13"
-
-  tags = {
-    Name = "postgres-db"
-  }
+  parameter_group_name = "default.postgres15"
+  skip_final_snapshot  = true
 }
